098d26a22517940e0f6904d2ac9433e7354a481f
Harish Narayanan <hnarayanan@gmail.com>
2015-11-22 21:47:27 +0000

    Play with the demos some more

--- static/js/thinkbot-demo.js
+++ static/js/thinkbot-demo.js
@@ -3,19 +3,25 @@ window.onload = function() {
     var r = new X.renderer3D();
     r.container = 'visualization';
     r.init();
+    r.interactor.config.MOUSEWHEEL_ENABLED = false;
+    r.interactor.init();
 
     var solution = new X.mesh();
     solution.file = '/img/projects/thinkbot/elasticity000000.vtk';
-    //solution.magicmode = true;
-    solution.color = [0.2, 0.2, 0.4];
-    solution.opacity = 0.9;
+    solution.magicmode = true;
+    solution.color = [0.5, 0.5, 0.5];
+    solution.opacity = 0.85;
   //  solution.color = [0.8, 0.3, 0.3];
 //    solution.opacity = 0.6;
     r.add(solution);
 
     // For elasticity
-    r.camera.position = [0, 0, 40];
+    r.camera.position = [0, 0, 35];
     r.camera.focus = [0, 0, 0];
+
+    // For gear
+//    r.camera.position = [0, 0, 1.55];
+//    r.camera.focus = [0, 0, 0];
 
     // For hyperelasticity
     // r.camera.position = [0, 0, 2];
