8977bfb2b2811356f47f2e9fbe41576ad3186576
Harish Narayanan <hnarayanan@gmail.com>
2015-11-22 21:00:41 +0000

    Continue to play with thinkbot demos

--- static/js/thinkbot-demo.js
+++ static/js/thinkbot-demo.js
@@ -1,29 +1,30 @@
 window.onload = function() {
 
-    // create and initialize a 3D renderer
     var r = new X.renderer3D();
     r.container = 'visualization';
     r.init();
 
-    // create a new X.mesh
-    var skull = new X.mesh();
-    // .. and associate the .vtk file to it
-    skull.file = '/img/projects/thinkbot/skull.vtk';
-    // .. make it transparent
-    skull.opacity = 1;
-    skull.magicmode = true;
-    // .. add the mesh
-    r.add(skull);
+    var solution = new X.mesh();
+    solution.file = '/img/projects/thinkbot/elasticity000000.vtk';
+    //solution.magicmode = true;
+    solution.color = [0.2, 0.2, 0.4];
+    solution.opacity = 0.9;
+  //  solution.color = [0.8, 0.3, 0.3];
+//    solution.opacity = 0.6;
+    r.add(solution);
 
-    // re-position the camera to face the skull
-    r.camera.position = [0, 250, 0];
+    // For elasticity
+    r.camera.position = [0, 0, 40];
+    r.camera.focus = [0, 0, 0];
+
+    // For hyperelasticity
+    // r.camera.position = [0, 0, 2];
+    // r.camera.focus = [0, 0, 0];
 
     // animate..
     r.onRender = function() {
 
-	// rotate the skull around the Z axis
-	// since we moved the camera, it is Z not X
-	skull.transform.rotateZ(1);
+	solution.transform.rotateY(0.4);
 
 	// we could also rotate the camera instead which is better in case
 	// we have a lot of objects and want to rotate them all:
